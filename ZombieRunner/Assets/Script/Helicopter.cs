3f24abee4cf59ef905f53cbb9dc394355ecb36fe
maciejg <maciej.gora01@gmail.com>
2019-02-25 18:24:38 +0100

    Helicopter Added

---
+++ ZombieRunner/Assets/Script/Helicopter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    private bool called = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("CallHelicopter")) {
+            if (!called) {
+                called = true; 
+                PlayHeliSound();
+            }           
+        }
+	}
+
+    private void PlayHeliSound() {
+        Debug.Log("Helicopter called");
+    }
+}
