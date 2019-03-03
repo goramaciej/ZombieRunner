4e2b98eabc2f11a9dc3f2e6817e3b170d8b8ea3e
maciejg <maciej.gora01@gmail.com>
2019-03-03 18:52:37 +0100

    NavMeshSurface test application added to new scene

---
+++ ZombieRunner/Assets/NavMeshAgentTest/NavMeshAgentGameSpecification.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshAgentGameSpecification : MonoBehaviour {
+
+    /* idea taken from:
+    https://www.youtube.com/watch?v=CHV1ymlw-P8
+
+
+    We've got a floor and "enemy" that can easily move between walls (obstacles);
+    By clicking mouse we set the point that the enemy has to reach. 
+    NavMesh building components must be installed from:
+    https://github.com/Unity-Technologies/NavMeshComponents
+
+
+    */
+
+
+
+}
